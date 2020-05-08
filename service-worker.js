f27daaf92e1307de2099b57a33e468f9bd6e04a1
kambleaa007 <ashishkamble191191@gmail.com>
2020-05-08 11:21:34 +0000

    Deploying to gh-pages from  @ 5aec5653a48d254395fab5df7225f747889ebd9c 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.451782ece782c47eeb6df264dd7385bc.js"
+  "/precache-manifest.ca07a0c44e8ee29df97eaea1126d0a64.js"
 );
 
 self.addEventListener('message', (event) => {
